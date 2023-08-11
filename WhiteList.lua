4fb88f6435e7cf8e2535cb89a49f3bcb1a33f925
UWUBeiFeng <108613872+UWUBeiFeng@users.noreply.github.com>
2023-08-11 12:13:33 +0800

    Update WhiteList.lua

--- WhiteList.lua
+++ WhiteList.lua
@@ -1,11 +1,5 @@
 whitelist = {
 	["CN_MIXUE"] = false,
-	["Imdinzhen"] = false,
-	["CN_long114514"] = false,
 	["myzqwert1"] = false,
-	["woyffw"] = false,
-	["Chinese2140"] = false,
-	["X7wl9TZUtt9P1bd"] = false,
-	["djbdksh3"] = false,
-	["gghbfffjsj"] = false
+	["xiaonuof"] = false,
 }
