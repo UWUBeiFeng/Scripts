34f86e92535279325a947afdaa9875c713114b05
UWUBeiFeng <108613872+UWUBeiFeng@users.noreply.github.com>
2023-08-12 21:19:33 +0800

    Update WhiteList.lua

--- WhiteList.lua
+++ WhiteList.lua
@@ -1,7 +1,12 @@
 whitelist = {
+	-- 付费账号
 	["CN_MIXUE"] = false,
 	["myzqwert1"] = false,
 	["xiaonuof"] = false,
+	["kaigez2"] = false,
+	["linsbfnn"] = false,
+
+	-- 公益账号
 	["djbdksh3"] = false,
 	["gsafxa26"] = false,
 	["dkfkfkfjfkfjdj"] = false,
